baa2d32dc2656b1c3910d5b94fc11777383b58a9
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-05 19:31:21 -0400

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const app = require('express')();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.listen(
-    PORT, () => console.log(`connected to http://localhost:${PORT}`)
-)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.get('/home',(req,res)=>{
-    res.status(200).send('hi')
-});
-
-app.post('/home',(req,res)=>{
-    
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
