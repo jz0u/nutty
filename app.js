017519bc699a7bce12fa2e11a8ac50803fadf207
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-05 19:21:32 -0400

    added get and post methods for homepage

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const PORT = 3000;
 app.listen(
     PORT, () => console.log(`connected to http://localhost:${PORT}`)
 )
+
+app.get('/home',(req,res)=>{
+    res.status(200).send('hi')
+});
+
+app.post('/home',(req,res)=>{
+    
+});
