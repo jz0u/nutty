5554527e107ba32b26e55550fab5104095882971
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-05 21:13:22 -0400

    implemeted try and catch in post of api/logs

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+const Log = require("./models/log.model.js");
 const PORT = 3000;
 const mongoose = require("mongoose");
 require("dotenv").config();
 const DB_URI = process.env.mongodb_uri;
 
-
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/api/logs", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/api/logs", async (req, res) => {
+  try { 
+    const log = await Log.create(req.body);
+    res.status(200).json(log);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
 
 mongoose
   .connect(DB_URI)
