ea72d8dbf880a7c293d1c9f85c38ee6c1b0ffe0f
Jackie Zou <jackiezou123@yahoo.com>
2025-08-06 14:43:20 -0400

    added hashing of password and storing into the db

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model.js");
 
 const get_users = async (req, res) => {
@@ -11,9 +12,11 @@ const get_users = async (req, res) => {
 
 const create_user = async (req,res)=>{
   console.log('Request body:', req.body);
-
   try {
-      const user = await User.create(req.body);
+      const password = req.body.password;
+      const hash = await bcrypt.hash(password,10);
+
+      const user = await User.create({...req.body,password: hash});
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ message: error.message });
