118650720a88c72db406a97c6fd667ce77c32780
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-05 23:19:48 -0400

    final clean ups and added a homepage

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const PORT = 3000;
 const log_route = require("./routes/log.route.js")
 const Log = require("./models/log.model.js");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 const DB_URI = process.env.mongodb_uri;
 
 // middleware
 app.use(express.json());
+
 // routes
 app.use("/api/logs",log_route);
 
+//pages
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "public/homepage/homepage.html"));
 });
 
 mongoose
