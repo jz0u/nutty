fdc38fe18edee6ae40219d0a0cf3ebd3826d42ed
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-05 20:56:22 -0400

    added post for api/logs

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.json());
+
 const PORT = 3000;
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const DB_URI = process.env.mongodb_uri;
 
-mongoose.connect(DB_URI)
-    .then(()=>{
-        console.log("\nconnected to atlas\n");
-        app.listen(
-        PORT, () => console.log(`\nserver running on http://localhost:${PORT}\n`));
-    })
-    .catch(()=>{
-        console.log("\nerror connecting to atlas\n");
-    });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.post("/api/logs", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("\nconnected to atlas\n");
+    app.listen(PORT, () =>
+      console.log(`\nserver running on http://localhost:${PORT}\n`)
+    );
+  })
+  .catch(() => {
+    console.log("\nerror connecting to atlas\n");
+  });
