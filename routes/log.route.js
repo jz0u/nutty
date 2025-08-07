61f2fc037cadcc40836383f26fcb19b982f07f74
Jackie Zou <82422936+jz0u@users.noreply.github.com>
2025-08-06 22:31:11 -0400

    cleaned log routes

--- routes/log.route.js
+++ routes/log.route.js
@@ -9,15 +9,12 @@ const {
   delete_log,
   get_logs_user,
 } = require("../controllers/log.controller.js");
+// api/logs/
 router.get("/user",get_logs_user); //get logs of user
 router.get("/", get_logs); //get all
-
 router.get("/:id", get_log); //get by id
-
 router.post("/", create_log); //create new entry
-
 router.put("/:id", edit_log); //edit by id
-
 router.delete("/:id", delete_log); //delete by id
 
 
